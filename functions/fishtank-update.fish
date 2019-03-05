284ac08b231d82d6a1df4279b86e9839cf6fafdb
Benji Hooper <benji@sixp.in>
2019-03-05 00:39:10 +0000

    Better update

--- functions/fishtank-update.fish
+++ functions/fishtank-update.fish
@@ -1,6 +1,9 @@
 function fishtank-update
-	echo "Hey"
+	echo "Updating functions"
 	if test -e functions/
 		cp functions/* $HOME/.config/fish/functions/
 	end
+
+	source ~/.config/fish/config.fish
+	source ~/.config/fish/functions/*
 end
