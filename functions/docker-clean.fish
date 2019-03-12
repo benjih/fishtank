c3e2bc3ca76906f745dea450b2f54a1ce909ef3e
Benji Hooper <benji@sixp.in>
2019-03-12 11:45:56 +0000

    Added docker-clean function

---
+++ functions/docker-clean.fish
@@ -0,0 +1,5 @@
+# Defined in /Users/benjih/.config/fish/config.fish @ line 60
+function docker-clean
+	docker volume rm (docker volume ls -qf dangling=true)
+	docker volume ls -qf dangling=true
+end
