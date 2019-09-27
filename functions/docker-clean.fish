3c4f1d6925939a6094ef9f33c243b0124ad10c40
Benji Hooper <benji@sixp.in>
2019-09-27 01:17:24 +0100

    Update docker-clean

--- functions/docker-clean.fish
+++ functions/docker-clean.fish
@@ -1,6 +1,4 @@
 # Defined in /Users/benjih/.config/fish/config.fish @ line 60
 function docker-clean
-	docker volume rm (docker volume ls -qf dangling=true)
-	docker rmi (docker images -q -f "dangling=true")
-	docker volume ls -qf dangling=true
+	docker system prune -f
 end
