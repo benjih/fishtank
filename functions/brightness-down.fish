ecf1168802e299d411b1c6f2646186256dc021da
Benji Hooper <benji@sixp.in>
2020-06-24 00:09:58 +0100

    Brightness up and down for Mac

---
+++ functions/brightness-down.fish
@@ -0,0 +1,3 @@
+function brightness-down
+	osascript -e 'tell application "System Events"' -e 'key code 144' -e ' end tell'	
+end
---
+++ functions/brightness-up.fish
@@ -0,0 +1,3 @@
+function brightness-up
+	osascript -e 'tell application "System Events"' -e 'key code 145' -e ' end tell'	
+end
